183778a23ac27569571c8923948b6f671c4f481f
Garen <garendemirdogen1@gmail.com>
2022-07-05 22:10:24 -0400

    added variables to use later, classes to use in index file later

---
+++ script.js
@@ -0,0 +1,8 @@
+var startButton = document.getElementById("start-button");
+var startContainer = document.getElementById("start-container");
+var testContainer = document.getElementById("test-container");
+var finishContainer = document.getElementById("finish-container");
+
+var questionMain = document.querySelector("#test-container .questions");
+var selection1 = document.querySelector("#test-container #selection1");
+var selection2 = document.querySelector("#test-container #selection2");
